57281358e7715b350560f6447cec81c4b0bbf2a2
Koki Yasuno <kokiyasuno45@gmail.com>
2022-12-25 21:12:22 +0900

    Update

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 (setq
  gc-cons-threshold (* 100 1024 1024)
  mac-command-modifier 'meta
+ mac-option-modifier 'none
  native-comp-async-report-warnings-errors nil
  read-process-output-max (* 1024 1024)
  split-height-threshold nil
@@ -37,6 +38,121 @@
   :after corfu
   :config)
 
+;; Example configuration for Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings (mode-specific-map)
+         ("C-c h" . consult-history)
+         ("C-c m" . consult-mode-command)
+         ("C-c k" . consult-kmacro)
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings (search-map)
+         ("M-s d" . consult-find)
+         ("M-s D" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key (kbd "M-.")
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;; There are multiple reasonable alternatives to chose from.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 3. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 4. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  )
+
 (use-package corfu
   :custom
   (corfu-auto t)
@@ -48,6 +164,7 @@
   (add-hook 'meow-insert-exit-hook #'meow-exit-corfu))
 
 (use-package denote
+  :disabled t
   :custom
   (denote-directory "~/org")
   (denote-dired-directories `(,denote-directory))
@@ -78,7 +195,7 @@
   (doom-themes-enable-bold t)
   (doom-themes-enable-italic t)
   :config
-  (load-theme 'doom-Iosvkem t))
+  (load-theme 'doom-solarized-dark-high-contrast t))
 
 (use-package eat
   :straight (eat :fetcher git
@@ -103,14 +220,43 @@
   (add-to-list 'eglot-server-programs
 	       '(typescript-tsx-mode "typescript-language-server" "--stdio")))
 
+
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 (use-package lsp-mode
+  :after orderless
   :custom
   (lsp-completion-provider :none)
+  (lsp-eslint-auto-fix-on-save t)
   (lsp-keymap-prefix "C-c l")
-  :hook ((ruby-mode
-	  typescript-mode
-	  typescript-tsx-mode)
-	 . lsp-deferred))
+  :preface
+  (defun my/lsp-mode-setup-completion ()
+    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+          '(orderless)))
+  :hook (((ruby-mode
+	   typescript-mode
+	   typescript-tsx-mode
+	   vue-mode)
+	  . lsp-deferred)
+	 (lsp-completion-mode . my/lsp-mode-setup-completion)))
 
 (use-package meow
   :config
@@ -228,24 +374,76 @@
 (use-package org
   :straight (:type built-in)
   :custom
+  (org-hide-emphasis-markers t)
   (org-log-done 'time)
+  (org-log-into-drawer t)
   (org-outline-path-complete-in-steps nil)
   (org-refile-targets
    '((org-agenda-files :maxlevel . 1)))
   (org-refile-use-outline-path 'file)
+  (org-startup-indented t)
   (org-todo-keywords '((sequence "TODO(t)" "PROG(p)" "|" "DONE(d)")
 		       (sequence "WAIT(w)" "HOLD(h)" "IDEA(i)" "|" "STOP(s@/!)")))
   (org-use-fast-todo-selection 'expert))
 
 (use-package org-agenda
   :straight (:type built-in)
+  :after s
   :custom
   (org-agenda-files `(,org-directory ,(concat (file-name-as-directory org-directory) "daily")))
-  :bind ("C-c A" . org-agenda))
+  (org-agenda-prefix-format
+   '((agenda . " %i %-12(org-agenda-category 12)%?-12t% s")
+     (todo . " %i %-12(org-agenda-category 12) ")
+     (tags . " %i %-12(org-agenda-category 12) ")
+     (search . " %i %-12(org-agenda-category 12) ")))
+  :preface
+  (defun org-buffer-property-get (name)
+    "Get a buffer property called NAME as a string."
+    (org-with-point-at 1
+      (when (re-search-forward (concat "^#\\+" name ": \\(.*\\)")
+                               (point-max) t)
+	(buffer-substring-no-properties
+	 (match-beginning 1)
+	 (match-end 1)))))
 
-;; Trying out denote
+  (defun org-agenda-category (&optional len)
+    "Get category of item at point for agenda.
+
+Category is defined by one of the following items:
+
+- CATEGORY property
+- TITLE keyword
+- TITLE property
+- filename without directory and extension
+
+When LEN is a number, resulting string is padded right with
+spaces and then truncated with ... on the right if result is
+longer than LEN.
+
+Usage example:
+
+  (setq org-agenda-prefix-format
+        '((agenda . \" %(vulpea-agenda-category) %?-12t %12s\")))
+
+Refer to `org-agenda-prefix-format' for more information."
+    (let* ((file-name (when buffer-file-name
+			(file-name-sans-extension
+			 (file-name-nondirectory buffer-file-name))))
+           (title (org-buffer-property-get "title"))
+           (category (org-get-category))
+           (result
+            (or (if (and
+                     title
+                     (string-equal category file-name))
+                    title
+                  category)
+		"")))
+      (if (numberp len)
+          (s-truncate len (s-pad-right len " " result))
+	result)))
+  :bind ("C-c a" . org-agenda))
+
 (use-package org-roam
-  :disabled t
   :custom
   (org-roam-capture-templates
    '(("d" "default" plain "%?" :target
@@ -259,13 +457,28 @@
 	 ("C-c n i" . org-roam-node-insert)
 	 ("C-c n c" . org-roam-capture)
 	 ;; Dailies
-	 ("C-c n j" . org-roam-dailies-capture-today))
+	 ("C-c n j" . org-roam-dailies-capture-today)
+	 ("C-c n t" . org-roam-dailies-goto-today))
   :config
   (org-roam-db-autosync-mode))
 
 (use-package org-modern
   :config
   (global-org-modern-mode))
+
+(use-package org-recur
+  :hook ((org-mode . org-recur-mode)
+         (org-agenda-mode . org-recur-agenda-mode))
+  :demand t
+  :config
+  (define-key org-recur-mode-map (kbd "C-c d") 'org-recur-finish)
+
+  ;; Rebind the 'd' key in org-agenda (default: `org-agenda-day-view').
+  (define-key org-recur-agenda-mode-map (kbd "d") 'org-recur-finish)
+  (define-key org-recur-agenda-mode-map (kbd "C-c d") 'org-recur-finish)
+
+  (setq org-recur-finish-done t
+        org-recur-finish-archive t))
 
 (use-package popwin
   :config
@@ -275,7 +488,7 @@
   :hook (typescript-tsx-mode . maybe-enable-prettier-js-mode)
   :init
   (defun maybe-enable-prettier-js-mode ()
-    (if (locate-dominating-file default-directory ".prettierrc")
+    (if (locate-dominating-file default-directory ".prettierrc.json")
 	(prettier-js-mode 1))))
 
 (use-package project
@@ -287,6 +500,8 @@
   :straight (:type built-in)
   :config
   (recentf-mode))
+
+(use-package s)
 
 (use-package savehist
   :straight (:type built-in)
@@ -308,14 +523,16 @@
   (tree-sitter-require 'tsx))
 
 (use-package typescript-mode
-  :mode (("\\.ts\\'" . typescript-mode)
-	 ("\\.tsx\\'" . typescript-tsx-mode))
-  :config
-  (define-derived-mode typescript-tsx-mode web-mode "TypeScript-TSX"))
+  :mode ("\\.ts\\'" . typescript-mode))
 
 (use-package vertico
   :config
   (vertico-mode))
 
 (use-package web-mode
-  :commands (web-mode))
+  :mode (("\\.tsx\\'" . typescript-tsx-mode)
+	 ("\\.vue\\'" . vue-mode))
+  :commands (web-mode)
+  :init
+  (define-derived-mode typescript-tsx-mode web-mode "TypeScript-TSX")
+  (define-derived-mode vue-mode web-mode "Vue"))
